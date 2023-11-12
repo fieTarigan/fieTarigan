ae046272905df814f8e1768ca7a6d3fc982ab301
Muhammad Ofie <ofieisranta@gmail.com>
2023-11-12 07:53:50 +0700

    fix: navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,39 +27,62 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={leagueSpartan.className}>
-        <main className="flex flex-col items-center bg-gray-700 text-white/75">
+        <main className="flex flex-col items-center bg-gray-700 text-white/75 w-[100%]">
           <div className="flex flex-col justify-between items-center min-h-screen xl:w-[60%] lg:w-[70%] md:w-[80%] w-[100%]">
-            <div className="flex justify-between items-center min-w-[100%] min-h-fit py-4 px-6 text-xl tracking-widest">
-              <Link
-                href="/"
-                className="pt-1 px-1 rounded-sm transition-colors duration-300 transform hover:bg-gray-500"
-              >
-                HOME
-              </Link>
-              <div className="flex gap-4 items-center">
-                <Link href="/experiences" className="pt-1 px-1 rounded-sm transition-colors duration-300 transform hover:bg-gray-500">
-                  EXPERIENCES
+            <div className="grid grid-cols-1 w-[100%] py-4 px-6 text-xl tracking-widest">
+              <div className="flex justify-between items-center ">
+                <Link
+                  href="/"
+                  className="pt-1 px-1 rounded-sm transition-colors duration-300 transform hover:bg-gray-500"
+                >
+                  HOME
                 </Link>
-                <Link href="/projects" className="pt-1 px-1 rounded-sm transition-colors duration-300 transform hover:bg-gray-500">
-                  PROJECTS
-                </Link>
-                {/* <Link href="/about" className="pt-1 px-1 rounded-sm transition-colors duration-300 transform hover:bg-gray-500">
+                <div className="flex gap-4 items-center">
+                  <Link
+                    href="/experiences"
+                    className="pt-1 px-1 rounded-sm transition-colors duration-300 transform hover:bg-gray-500 hidden xs:block"
+                  >
+                    EXPERIENCES
+                  </Link>
+                  <Link
+                    href="/projects"
+                    className="pt-1 px-1 rounded-sm transition-colors duration-300 transform hover:bg-gray-500 hidden xs:block"
+                  >
+                    PROJECTS
+                  </Link>
+                  {/* <Link href="/about" className="pt-1 px-1 rounded-sm transition-colors duration-300 transform hover:bg-gray-500">
                   ABOUT
                 </Link> */}
 
-                <a href="https://www.hackerrank.com/profile/ofieisranta">
-                  <Image src={iconHackerRank} alt="github icon" width={30} />
-                </a>
-                <a href="https://github.com/fieTarigan">
-                  <Image src={iconGithub} alt="github icon" width={30} />
-                </a>
-                <a href="https://www.linkedin.com/in/muhammad-ofie-640331175/">
-                  <Image src={iconLinkedIn} alt="linkedin icon" width={35} />
-                </a>
+                  <a href="https://www.hackerrank.com/profile/ofieisranta">
+                    <Image src={iconHackerRank} alt="github icon" width={30} />
+                  </a>
+                  <a href="https://github.com/fieTarigan">
+                    <Image src={iconGithub} alt="github icon" width={30} />
+                  </a>
+                  <a href="https://www.linkedin.com/in/muhammad-ofie-640331175/">
+                    <Image src={iconLinkedIn} alt="linkedin icon" width={35} />
+                  </a>
+                </div>
+              </div>
+
+              <div className="flex justify-around items-center xs:hidden pt-2">
+                <Link
+                  href="/experiences"
+                  className="pt-1 px-1 rounded-sm transition-colors duration-300 transform hover:bg-gray-500"
+                >
+                  EXPERIENCES
+                </Link>
+                <Link
+                  href="/projects"
+                  className="pt-1 px-1 rounded-sm transition-colors duration-300 transform hover:bg-gray-500"
+                >
+                  PROJECTS
+                </Link>
               </div>
             </div>
 
-            <div className="min-w-[100%]" >{children}</div>
+            <div className="min-w-[100%]">{children}</div>
 
             <div className="flex items-center justify-center gap-4 py-3">
               <span className="pt-1 text-lg">Built with &hearts; using</span>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss'
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 const config: Config = {
   content: [
@@ -14,6 +15,10 @@ const config: Config = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    screens: {
+      'xs': '550px',
+      ...defaultTheme.screens,
+    }
   },
   plugins: [],
 }
